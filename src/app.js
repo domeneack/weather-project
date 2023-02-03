3e8cfc2146a10b4fed8d12356846febf6e41733c
domeneack <112799387+domeneack@users.noreply.github.com>
2023-02-02 21:54:36 -0800

    Forecast apiUrl and apiKey added to JS
    new functions
    -getForecast
    -displayForcast
    -forecastHTML

--- src/app.js
+++ src/app.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+    console.log(response.data.daily);
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+        forecastHTML =
+          forecastHTML +
+          `<div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" width="36" />`;
+    })
+}
+function getForecast(coordinates) {
+  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -43,6 +65,8 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
